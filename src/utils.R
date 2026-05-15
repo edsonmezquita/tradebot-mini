4f0d29365be98022046448b6b6a371748cee634a
dereckmezquita <dereck@mezquita.io>
2026-05-14 22:07:00 -0600

    Loop.

---
+++ src/utils.R
@@ -0,0 +1,11 @@
+#' @export
+setInterval <- function(fun, interval = 60) {
+  handle <- later$later(
+    function() {
+      fun()
+      setInterval(fun, interval)
+    },
+    interval
+  )
+  return(invisible(handle))
+}
